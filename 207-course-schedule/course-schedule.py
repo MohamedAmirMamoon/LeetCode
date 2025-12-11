67637acd6180ec7b6550c9311289166b78284285
Mohamed Mamoon <146618803+MohamedAmirMamoon@users.noreply.github.com>
2025-12-10 20:50:29 -0800

    Time: 7 ms (46.35%) | Memory: 19.5 MB (22.58%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,28 +1,36 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        preMap = {i: [] for i in range(numCourses)}
-        for crs, pre in prerequisites:
-            preMap[crs].append(pre)
+        preMap = {}
+        visited = set()
 
-        # Store all courses along the current DFS path
-        visiting = set()
+        for i in range(numCourses):
+            preMap[i] = []
+
+        for crs, preReq in prerequisites:
+            preMap[crs].append(preReq)
+
+        # now we have initialized preMap
 
         def dfs(crs):
-            if crs in visiting:
-                # Cycle detected
-                return False
-            if preMap[crs] == []:
+            if crs in visited:
+                return False # proves it is a cycle
+            if len(preMap[crs]) == 0:
                 return True
 
-            visiting.add(crs)
-            for pre in preMap[crs]:
-                if not dfs(pre):
+            visited.add(crs)
+            # now we go through each prereq
+            for preReq in preMap[crs]:
+                if not dfs(preReq):
                     return False
-            visiting.remove(crs)
+            # why this
+            visited.remove(crs)
+
             preMap[crs] = []
             return True
 
-        for c in range(numCourses):
-            if not dfs(c):
+        # now go through all courses, and run dfs on each to see whatever it maps to is correct and does not return false
+        for crs in range(numCourses):
+            if not dfs(crs):
                 return False
+            
         return True
