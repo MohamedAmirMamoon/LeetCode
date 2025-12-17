475f606f4e861e822c638ee1aa76d2d6196f2d3f
Mohamed Mamoon <146618803+MohamedAmirMamoon@users.noreply.github.com>
2025-12-17 11:54:04 -0800

    Time: 0 ms (100.00%) | Memory: 18.1 MB (77.33%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,38 +1,35 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        adj = {}
+        parent = []
         for i in range(len(edges) + 1):
-            adj[i] = []
+            parent.append(i)
+
+        def findRoot(node):
+            nonlocal parent
+            if parent[node] != node:
+                return findRoot(parent[node])
+            return parent[node] 
+
+        def union(edge1, edge2):
+            nonlocal parent
+            
+            root1 = findRoot(edge1)
+            root2 = findRoot(edge2)
+            if root1 == root2:
+                return [edge1, edge2]
     
+            
+            parent[root2] = root1
 
-        edge = []
-        cycleFound = False
-        visited = set()
-        def dfs(node, par):
-            nonlocal cycleFound, edge
-    
-            if visit[node]:
-                return
+            return []
 
-            visit[node] = True
-
-            for newNode in adj[node]:
-                if newNode == par:
-                    continue
-                if visit[newNode]:
-                    # mark 
-                    cycleFound = True
-                    edge = [newNode, node]
-                    return
-                dfs(newNode, node)
-
-        for n1, n2 in edges:
-            adj[n1].append(n2)
-            adj[n2].append(n1)
-            visit = [False] * (len(edges)+1)
-
-            dfs(n1, -1)
-            if cycleFound:
-                return [n1, n2]
-
+        for u, v in edges:
+            edge = union(u, v)
+            if edge != []:
+                return edge
+        
+        # Input: edges = [[1,2],[1,3],[3,4],[2,4]]
+        # if parent of 4 is 1, and 3 is 3
+        # visited = 1, 2, 4, 3
+        # Output: [2,4]
         return []
