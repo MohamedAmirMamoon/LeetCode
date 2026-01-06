13e9a6412fb04f565b1bba08240398b2cca5c732
Mohamed Mamoon <146618803+MohamedAmirMamoon@users.noreply.github.com>
2026-01-05 20:03:59 -0500

    Time: 405 ms (48.04%) | Memory: 19.1 MB (70.55%) - LeetSync

--- 753-open-the-lock/open-the-lock.py
+++ 753-open-the-lock/open-the-lock.py
@@ -17,10 +17,10 @@ class Solution:
         # we will flip throguh each digit and go level by level order to find a shortest distance
         # we will make one turn at a time and add to queue
         q.append(("0000", 0))
-        amountMoves = 0
-        directions = [1, -1]
+    
         visited = set()
         dead = set(deadends)
+        amountMoves = 0
 
         if "0000" in dead:
             return -1
@@ -32,12 +32,26 @@ class Solution:
 
             # we generate 8 neighbors
             for i in range(4):
-                for d in directions:
-                    newDig = (int(code[i]) + d) % 10
-                    newCode = code[:i] + str(newDig) + code[i+1:]
-                    if newCode not in dead and newCode not in visited:
-                        q.append((newCode, layer + 1))
-                        visited.add(newCode)
+                # we are iterating through each string number in lock
+                # and we increment and decrement each
+                c = code[i]
+                c1 = (int(c) + 1) % 10
+                c2 = (int(c) - 1) % 10
+                newCode1 = code[:i] + str(c1) + code[i+1:]
+                newCode2 = code[:i] + str(c2) + code[i+1:]
+
+                if newCode1 not in dead and newCode1 not in visited:
+                    q.append((newCode1, layer+1))
+                    visited.add(newCode1)
+                if newCode2 not in dead and newCode2 not in visited:
+                    q.append((newCode2, layer+1))
+                    visited.add(newCode2)
+        # 0000
+        # 1000
+        # 9000
+        # 0100
+        # 0900
+                
 
         return -1
 
