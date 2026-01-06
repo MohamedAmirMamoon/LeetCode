d0210f95f878f791ed0656e3709dd7a3f82c614d
Mohamed Mamoon <146618803+MohamedAmirMamoon@users.noreply.github.com>
2026-01-05 19:07:52 -0500

    Time: 306 ms (40.21%) | Memory: 18 MB (99.82%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -17,51 +17,27 @@ class Solution:
         if endWord not in wordList:
             return 0
 
-        # we create adj
-        # why do we need adjc
-        # iterate through neibors in adj
-        # append to queue
-        # hit hot
+        # we can take advantage of the dict of wordList
+        # when in dfs, we take our word, iterate through all letters for each char
+        # if that exists in wordList then add it with one extra step
         # 
-        
-        wordList.append(beginWord)
-        adj = {}
-        for word in wordList:
-            adj[word] = []
-        n = len(wordList)
-        for i in range(n):
-            for j in range(i+1, n):
-                word = wordList[i]
-                wordtwo = wordList[j]
-                numDiff = 0
-                if word == wordtwo:
-                    continue
-                for t in range(len(word)):
-                    if word[t] != wordtwo[t]:
-                        numDiff += 1
-                # now if only one char diff add to adj
-                if numDiff == 1:
-                    adj[word].append(wordtwo)
-                    adj[wordtwo].append(word)
-
-        # now we do bfs
-        # we give input beginWord, and it checks each nei adding depth and then we return depth if we get to end word
 
         q = deque()
         q.append((beginWord, 1))
-        visited = set(beginWord)
+        wordSet = set(wordList)
 
         while q:
-            # now we check their adj neighbors
             word, tMoves = q.popleft()
             if word == endWord:
                 return tMoves
 
-            
-            for nei in adj[word]:
-                if nei not in visited:
-                    visited.add(nei)
-                    q.append((nei, tMoves+1))
+            for i in range(len(word)):
+                for c in "abcdefghijklmnopqrstuvwxyz":
+                    newWord = word[:i] + c + word[i+1:]
+                    if newWord in wordSet:
+                        wordSet.remove(newWord)
+                        q.append((newWord, tMoves+1))
+        
             
         return 0
                 
