f12ed2353f344a397df1e5e833a5a1672b45dc39
Mohamed Mamoon <146618803+MohamedAmirMamoon@users.noreply.github.com>
2026-04-03 09:22:47 -0700

    Time: 3 ms (52.94%) | Memory: 20.2 MB (66.54%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,15 +1,23 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}
+        # given nums
+        # target
+        # return two indexes that add up to target
+        # each input would have exactly one sol
 
+        # we could just do a nested for loop, would be super inefficient, but we would get our answer
+        # may not use same element twice, so no index used twice
+
+        # we can go through the array and as we go, store the index with the item in a map
+        # then if we find the compliment of another item existing in this map, we can return the indices of both
+        seen = {}
         for i in range(len(nums)):
             compliment = target - nums[i]
-
             if compliment in seen:
                 return [seen[compliment], i]
-            else:
-                seen[nums[i]] = i
 
+            seen[nums[i]] = i
+        
 
 
 
