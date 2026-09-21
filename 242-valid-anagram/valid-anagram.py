c838a2d6e42f188be4432c57299a0dfa3a08dc69
Amir Mamoon <146618803+MohamedAmirMamoon@users.noreply.github.com>
2026-09-20 22:21:40 -0700

    Time: 11 ms (77.58%) | Memory: 19.5 MB (46.19%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # two strings
+        # if both anagrams of eachother return True
+        # if not False
+
+        # same amount for each specific char
+
+        # iterate through one add to hashMap with value
+        # as we iterate through second remove each char from map 
+        # by end if map empty return True
+        # otherwise return False
+
+        # basecase: strings diff len, return False
+
+        if len(s) != len(t):
+            return False
+
+
+        charMap = defaultdict(int)
+        
+        for c in s:
+            charMap[c] += 1
+
+        for c in t:
+            charMap[c] -= 1
+            if charMap[c] == 0:
+                del charMap[c]
+        
+        print(charMap)
+        if not charMap:
+            return True
+
+        return False
