6f3bd5992b1eb5b9b36af893e632b9d1c8974f33
Amir Mamoon <146618803+MohamedAmirMamoon@users.noreply.github.com>
2026-09-24 14:57:13 -0700

    Time: 27 ms (5.21%) | Memory: 26.8 MB (6.61%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,16 +1,40 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # array nums
-        # k most freq elements
-        hashSet = defaultdict(int)
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        # nums and k, return the k most frequent
+        # we want to track the most frequent elements
+        # figure out a way to count each element
+
+        # maybe a heap?
+
+        # with an array we iterate through
+        # we can store a default dict and sort by values? and return the top 3 with an array?
+        # O(n) by adding to map
+        # instead of sort we just add by value wiht a hash
+        # 1: 3 times, 2: 1 time(s), 3: 7 times
+
+        # we can add to minheap and if already seen avoid it and do the next one
+        heap = []
+        heapq.heapify(heap)
+        countNum = defaultdict(int)
+
         for num in nums:
-            hashSet[num] += 1
+            countNum[num] -= 1
+            heapq.heappush(heap, [countNum[num], num])
+
+        result = []
         
-        sortedList = sorted(hashSet, key=lambda x: hashSet[x], reverse=True)
+        alreadySeen = set()
+        i = 0
+        while i != k:
+            num = -heapq.heappop(heap)[1]
+            print(num)
+            if num not in alreadySeen:
+                result.append(-1*num)
+                alreadySeen.add(num)
+                i += 1
+            
 
-        finalList = []
-        for i in range(k):
-            finalList.append(sortedList.pop(0))
+            
 
-        return finalList
-
+            
+        return result
